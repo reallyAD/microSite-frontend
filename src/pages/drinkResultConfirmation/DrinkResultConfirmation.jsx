23e2d351fef266a7c1710482b608f91cfa3b5779
Jxredkong <jkong011@e.ntu.edu.sg>
2025-05-10 19:31:38 +0800

    Horizontal drinkresultconfirmation

--- src/pages/drinkResultConfirmation/DrinkResultConfirmation.jsx
+++ src/pages/drinkResultConfirmation/DrinkResultConfirmation.jsx
@@ -5,6 +5,8 @@ import ResusableButton from '../../components/ReusableButton';
 import LoadingPage from "../../components/LoadingWizard";
 import witch from "../../assets/images/witch.gif"
 import { useLocation } from 'react-router-dom';
+import orangebottle from '../../assets/images/orangebottle.png';
+import { orange } from '@mui/material/colors';
 
 function DrinkResultConfirmation() {
 
@@ -35,36 +37,44 @@ function DrinkResultConfirmation() {
 
     // TODO: Dynamically load data from output of chatgpt
     return (
-      <>
-        <BackButton />
-        <div className="h-screen text-3xl text-bold flex justify-center items-center">
+    <>
+      <BackButton />
+
+      <div className="h-screen bg-black text-white p-4 flex flex-col items-center justify-center transform scale-110">
+
+        {/* Main content */}
+        <div className="w-full max-w-4xl flex justify-between">
+
+          {/* Left column */}
           <div className="flex flex-col items-center">
-            <p className="text-2xl"> Your Drink</p>
-            {/* TODO: Dynamically load in data from Chatgpt */}
-            <h1 className="font-bold">
-              <span className="text-deepOrange">{drinkDetails.name}</span>
-            </h1>
-            <p className="my-20"> INSERT IMAGE </p>
-            <div className="w-8/12">
-              <p className="text-lg">{drinkDetails.description}</p>
+            <span className="font-bold text-4xl">Your Drink</span>
+            <img src={orangebottle} alt="Generated Drink" className="w-72 h-auto mt-4 rotate-15" />
+            <h2 className="text-4xl font-bold text-deepOrange ">
+              {drinkDetails.name}
+            </h2>
+          </div>
+
+          {/* Right column */}
+          <div className="w-1/2 text-center space-y-10 flex flex-col items-center">
+            <div className=" flex flex-col items-center">
+              <span className="font-bold text-deepOrange mb-1 text-xl"> Description:</span>
+              <span className="mt-1 text-lg">{drinkDetails.description}</span>
             </div>
 
-            <div className="flex flex-row mt-10 justify-between">
-              <div className="w-5/12">
-                <p className="font-bold">
-                  <span className="text-deepOrange"> Ingredients </span>
-                </p>
-                <p className="text-xs">{drinkDetails.ingredients.join(", ")}</p>
+            <div className="flex justify-center text-sm w-full">
+              <div className="w-1/2 pr-4 flex flex-col items-center">
+                <span className="font-bold text-deepOrange mb-1 text-xl">Ingredients:</span>
+                <span className="text-lg text-center mt-2">{drinkDetails.ingredients.join(', ')}</span>
               </div>
-              <div>
-                <p className="font-bold">
-                  <span className="text-deepOrange"> Taste Profile </span>
-                </p>
-                <p className="text-xs">{drinkDetails.taste_profile.join(", ")}</p>
+              <div className="w-1/2 pl-4 flex flex-col items-center">
+                <span className="font-bold text-deepOrange mb-1 text-xl">Taste Notes:</span>
+                <span className="text-lg text-center mt-2">{drinkDetails.taste_profile.join(', ')}</span>
               </div>
             </div>
-            <div>
-              <div className="flex flex-row justify-center items-center gap-x-2 mt-8">
+
+            {/* Buttons */}
+            <div className="mt-6 flex flex-col items-center gap-4">
+              <div className="flex gap-4 justify-center">
                 <ResusableButton
                   onClick={handleOnClick}
                   text="Re-Roll"
@@ -72,7 +82,7 @@ function DrinkResultConfirmation() {
                   width={140}
                   height={40}
                 />
-                <ResusableButton  
+                <ResusableButton
                   onClick={handleOnClick}
                   text="Refine"
                   color="green"
@@ -80,20 +90,20 @@ function DrinkResultConfirmation() {
                   height={40}
                 />
               </div>
-              <div className="flex justify-center items-center mt-2">
-                <ResusableButton
-                  onClick={handleOnClick}
-                  text="Confirm"
-                  color="pink"
-                  width={288}
-                  height={40}
-                />
-              </div>
+              <ResusableButton
+                onClick={handleOnClick}
+                text="Confirm"
+                color="pink"
+                width={288}
+                height={40}
+              />
             </div>
-          </div>  
+          </div>
         </div>
-     </>
-    )
+      </div>
+    </>
+);
+
   }
   
   export default DrinkResultConfirmation;

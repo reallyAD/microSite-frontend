96102880d496289ddf448e646a52122a4d71c9d1
Jeremy <e1155908@u.nus.edu>
2025-05-24 18:41:52 +0800

    change some css

--- src/pages/drinkResult/DrinkResult.jsx
+++ src/pages/drinkResult/DrinkResult.jsx
@@ -64,72 +64,74 @@ function DrinkResult() {
                             />
                         </div>
                     </div>
-        
+
                     {/* Right Panel */}
                     <div className="w-full md:w-1/2 h-full flex flex-col justify-center items-center bg-zinc-900 p-6 sm:p-8 md:p-10 rounded-3xl shadow-lg">
-                        <div className="flex flex-col items-center w-full h-full justify-center">
+                        <div className="flex flex-col items-center w-full h-full justify-center gap-y-10">
                             <h2 className="text-3xl font-bold text-center mb-4 leading-tight">
                                 Let’s make this drink a reality!
                             </h2>
-                            <p className="text-sm text-zinc-300 mb-6 text-center max-w-sm">
-                                Insert your email and we’ll reach out to you within 1–2 working days.
-                            </p>
-                            <TextField
-                                label="Email"
-                                variant="outlined"
-                                size="small"
-                                onChange={handleChange}
-                                sx={{
-                                    width: '100%',
-                                    maxWidth: 320,
-                                    mb: 2,
-                                    backgroundColor: 'lightgray',
-                                    borderRadius: '4px',
-                                    '& .MuiOutlinedInput-root': {
-                                    '& fieldset': {
-                                        borderColor: isFilled ? '#FF640A' : 'lightgray',
-                                        borderWidth: isFilled ? '2px' : '1px',
-                                    },
-                                    '&:hover fieldset': {
-                                        borderColor: isFilled ? '#FF640A' : 'lightgray',
-                                    },
-                                    '&.Mui-focused fieldset': {
-                                        borderColor: '#FF640A',
-                                    },
-                                    },
-                                    '& .MuiInputLabel-root': {
-                                    color: isFilled ? '#FF640A' : 'black',
-                                    },
-                                    '& .MuiInputLabel-root.Mui-focused': {
-                                    color: '#FF640A',
-                                    },
-                                }}
-                                />
-
-                            <Button
-                                variant="contained"
-                                onClick={handleSubmission}
-                                disabled={!isFilled}
-                                sx={{
-                                    backgroundColor: isFilled ? '#FF640A' : 'lightgray',
-                                    color: isFilled ? 'black' : 'gray',
-                                    fontWeight: 'bold',
-                                    padding: '10px 28px',
-                                    mt: 1,
-                                    borderRadius: '8px',
-                                    fontSize: '0.9rem',
-                                    textTransform: 'none',
-                                    '&:hover': {
-                                        backgroundColor: isFilled ? '#A04000' : 'lightgray',
-                                    },
-                                    '&.Mui-disabled': {
+                            <div className="items-center flex flex-col">
+                                <p className="text-sm text-zinc-300 mb-6 text-center max-w-sm">
+                                    Insert your email and we’ll reach out to you within 1–2 working days.
+                                </p>
+                                <TextField
+                                    label="Email"
+                                    variant="outlined"
+                                    size="small"
+                                    onChange={handleChange}
+                                    sx={{
+                                        width: '100%',
+                                        maxWidth: 320,
+                                        mb: 2,
                                         backgroundColor: 'lightgray',
-                                        color: 'black',
-                                    },
-                                }}
-                            >
-                                Submit
-                            </Button>
+                                        borderRadius: '4px',
+                                        '& .MuiOutlinedInput-root': {
+                                        '& fieldset': {
+                                            borderColor: isFilled ? '#FF640A' : 'lightgray',
+                                            borderWidth: isFilled ? '2px' : '1px',
+                                        },
+                                        '&:hover fieldset': {
+                                            borderColor: isFilled ? '#FF640A' : 'lightgray',
+                                        },
+                                        '&.Mui-focused fieldset': {
+                                            borderColor: '#FF640A',
+                                        },
+                                        },
+                                        '& .MuiInputLabel-root': {
+                                        color: isFilled ? '#FF640A' : 'black',
+                                        },
+                                        '& .MuiInputLabel-root.Mui-focused': {
+                                        color: '#FF640A',
+                                        },
+                                    }}
+                                    />
+
+                                <Button
+                                    variant="contained"
+                                    onClick={handleSubmission}
+                                    disabled={!isFilled}
+                                    sx={{
+                                        backgroundColor: isFilled ? '#FF640A' : 'lightgray',
+                                        color: isFilled ? 'black' : 'gray',
+                                        fontWeight: 'bold',
+                                        padding: '10px 28px',
+                                        mt: 1,
+                                        borderRadius: '8px',
+                                        fontSize: '0.9rem',
+                                        textTransform: 'none',
+                                        '&:hover': {
+                                            backgroundColor: isFilled ? '#A04000' : 'lightgray',
+                                        },
+                                        '&.Mui-disabled': {
+                                            backgroundColor: 'lightgray',
+                                            color: 'black',
+                                        },
+                                    }}
+                                >
+                                    Submit
+                                </Button>
+                            </div>
                         </div>
                     </div>
                 </div>

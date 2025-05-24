7c475c9a8b0c85026593d9513aa9af1488123a71
Jeremy Sim <142785197+jereeemyyyy@users.noreply.github.com>
2025-05-24 18:43:03 +0800

    Update/drinkresult (#50)
    * Integrated new bottle image into drinkResult page
    
    * change some css

--- src/pages/drinkResult/DrinkResult.jsx
+++ src/pages/drinkResult/DrinkResult.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import BackButton from '../../components/BackButton';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import orangebottle from '../../assets/images/orangebottle.png';
-import { NoEncryption } from '@mui/icons-material';
+
+// Dynamically import bottle images
+const bottleImages = import.meta.glob('../../assets/images/*.jpg', { eager: true , import: 'default'});
 
 function DrinkResult() {
-    const [isFilled, setIsFilled] = React.useState(false);
+    const [isFilled, setIsFilled] = useState(false);
+    const [imageSrc, setImageSrc] = useState(null);
     const location = useLocation();
     const generatedDrink = location.state || {};
+
+    const bottleImageKey = `../../assets/images/${generatedDrink.bottle_color}.jpg`;
+
+    useEffect(() => {
+        console.log("Bottle Image Key: ", bottleImageKey);
+        if (bottleImages[bottleImageKey]) {
+            setImageSrc(bottleImages[bottleImageKey]);
+        } else {
+            console.error(`Image not found for color: ${generatedDrink.bottle_color}`);
+        }
+    }, [bottleImageKey]);
 
     const handleChange = (e) => {
         const trimmedValue = e.target.value.trim();
@@ -23,6 +36,8 @@ function DrinkResult() {
         category: generatedDrink.category,
         description: generatedDrink.description || "A refreshing twist for your taste buds!",
     };
+
+    
 
     return (
         <>
@@ -43,78 +58,80 @@ function DrinkResult() {
                         </div>
                         <div className="flex-grow flex items-center justify-center mt-2">
                             <img
-                                src={orangebottle}
+                                src={imageSrc}
                                 alt="Generated Drink"
-                                className="w-36 h-auto transform rotate-[15deg] transition-transform duration-300 hover:scale-110"
+                                className="w-72 h-auto transform rotate-[15deg] transition-transform duration-300 hover:scale-110"
                             />
                         </div>
                     </div>
 
                     {/* Right Panel */}
                     <div className="w-full md:w-1/2 h-full flex flex-col justify-center items-center bg-zinc-900 p-6 sm:p-8 md:p-10 rounded-3xl shadow-lg">
-                        <div className="flex flex-col items-center w-full h-full justify-center">
+                        <div className="flex flex-col items-center w-full h-full justify-center gap-y-10">
                             <h2 className="text-3xl font-bold text-center mb-4 leading-tight">
                                 Let’s make this drink a reality!
                             </h2>
-                            <p className="text-sm text-zinc-300 mb-6 text-center max-w-sm">
-                                Insert your email and we’ll reach out to you within 1–2 working days.
-                            </p>
-                            <TextField
-                                label="Email"
-                                variant="outlined"
-                                size="small"
-                                onChange={handleChange}
-                                sx={{
-                                    width: '100%',
-                                    maxWidth: 320,
-                                    mb: 2,
-                                    backgroundColor: 'lightgray',
-                                    borderRadius: '4px',
-                                    '& .MuiOutlinedInput-root': {
-                                    '& fieldset': {
-                                        borderColor: isFilled ? '#FF640A' : 'lightgray',
-                                        borderWidth: isFilled ? '2px' : '1px',
-                                    },
-                                    '&:hover fieldset': {
-                                        borderColor: isFilled ? '#FF640A' : 'lightgray',
-                                    },
-                                    '&.Mui-focused fieldset': {
-                                        borderColor: '#FF640A',
-                                    },
-                                    },
-                                    '& .MuiInputLabel-root': {
-                                    color: isFilled ? '#FF640A' : 'black',
-                                    },
-                                    '& .MuiInputLabel-root.Mui-focused': {
-                                    color: '#FF640A',
-                                    },
-                                }}
-                                />
-
-                            <Button
-                                variant="contained"
-                                onClick={handleSubmission}
-                                disabled={!isFilled}
-                                sx={{
-                                    backgroundColor: isFilled ? '#FF640A' : 'lightgray',
-                                    color: isFilled ? 'black' : 'gray',
-                                    fontWeight: 'bold',
-                                    padding: '10px 28px',
-                                    mt: 1,
-                                    borderRadius: '8px',
-                                    fontSize: '0.9rem',
-                                    textTransform: 'none',
-                                    '&:hover': {
-                                        backgroundColor: isFilled ? '#A04000' : 'lightgray',
-                                    },
-                                    '&.Mui-disabled': {
+                            <div className="items-center flex flex-col">
+                                <p className="text-sm text-zinc-300 mb-6 text-center max-w-sm">
+                                    Insert your email and we’ll reach out to you within 1–2 working days.
+                                </p>
+                                <TextField
+                                    label="Email"
+                                    variant="outlined"
+                                    size="small"
+                                    onChange={handleChange}
+                                    sx={{
+                                        width: '100%',
+                                        maxWidth: 320,
+                                        mb: 2,
                                         backgroundColor: 'lightgray',
-                                        color: 'black',
-                                    },
-                                }}
-                            >
-                                Submit
-                            </Button>
+                                        borderRadius: '4px',
+                                        '& .MuiOutlinedInput-root': {
+                                        '& fieldset': {
+                                            borderColor: isFilled ? '#FF640A' : 'lightgray',
+                                            borderWidth: isFilled ? '2px' : '1px',
+                                        },
+                                        '&:hover fieldset': {
+                                            borderColor: isFilled ? '#FF640A' : 'lightgray',
+                                        },
+                                        '&.Mui-focused fieldset': {
+                                            borderColor: '#FF640A',
+                                        },
+                                        },
+                                        '& .MuiInputLabel-root': {
+                                        color: isFilled ? '#FF640A' : 'black',
+                                        },
+                                        '& .MuiInputLabel-root.Mui-focused': {
+                                        color: '#FF640A',
+                                        },
+                                    }}
+                                    />
+
+                                <Button
+                                    variant="contained"
+                                    onClick={handleSubmission}
+                                    disabled={!isFilled}
+                                    sx={{
+                                        backgroundColor: isFilled ? '#FF640A' : 'lightgray',
+                                        color: isFilled ? 'black' : 'gray',
+                                        fontWeight: 'bold',
+                                        padding: '10px 28px',
+                                        mt: 1,
+                                        borderRadius: '8px',
+                                        fontSize: '0.9rem',
+                                        textTransform: 'none',
+                                        '&:hover': {
+                                            backgroundColor: isFilled ? '#A04000' : 'lightgray',
+                                        },
+                                        '&.Mui-disabled': {
+                                            backgroundColor: 'lightgray',
+                                            color: 'black',
+                                        },
+                                    }}
+                                >
+                                    Submit
+                                </Button>
+                            </div>
                         </div>
                     </div>
                 </div>

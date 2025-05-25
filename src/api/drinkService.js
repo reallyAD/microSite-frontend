68b2e0d04d1a7c36664fe8879bfdb45bdd340474
Jxredkong <jkong011@e.ntu.edu.sg>
2025-05-25 12:18:30 +0800

    change

--- src/api/drinkService.js
+++ src/api/drinkService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // To be changed to the actual API URL
-const API_BASE_URL = "http://localhost:8000";
+const API_BASE_URL = "https://microsite-backend.onrender.com";
 
 // Create axios instance
 const apiClient = axios.create({

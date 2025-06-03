1303f095e7c1529b120cf2a11dbd92547ff4f0e6
Jeremy <e1155908@u.nus.edu>
2025-06-03 21:41:24 +0800

    Change loading screen

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,14 +1,18 @@
 import { CircularProgress } from "@mui/material"
+import Lottie from "lottie-react"
+import loadingAnimation from "../assets/loading-animation-orange.json"
+
 
 const LoadingPage = ({message= 'Processing'}) => {
     return (
         <div className="text-white flex flex-col items-center justify-center h-screen bg-black">
-            <CircularProgress
+            {/* <CircularProgress
                 sx={{
                     color: 'white'
                 }}
                 size={82}
-            />
+            /> */}
+            <Lottie animationData={loadingAnimation} loop={true} className="w-100 h-100 -mt-24 -mb-24" />
             <span className="mt-4 text-xl font-medium">{message}</span>
         </div>
     )
